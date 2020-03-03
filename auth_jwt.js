b3d57507d68e9ea3052a1acf5d394ee321f886d5
Ricky Le <rickyle327@gmail.com>
2020-03-03 07:06:52 -0700

    Reset

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,4 +1,21 @@
-let passport = require('passport');
-let JwtStrategy = require('passport-jwt').Strategy;
-let ExtractJwt = require('passport-jwt').ExtractJwt;
+// Load required packages
+var passport = require('passport');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+opts.secretOrKey = process.env.SECRET_KEY;
+
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    var user = db.find(jwt_payload.id);
+
+    if (user) {
+        done(null, user);
+    } else {
+        done(null, false);
+    }
+}));
+
+exports.isAuthenticated = passport.authenticate('jwt', { session : false });
+exports.secret = opts.secretOrKey ;
